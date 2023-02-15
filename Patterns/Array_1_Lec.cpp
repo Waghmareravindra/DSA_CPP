33110b7592c7c97ffe435eb2191de874d6ff2901
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-02-15 23:38:25 +0530

    updated

---
+++ Patterns/Array_1_Lec.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+// array initialized
+
+  // int arr[]={1,2,3,4,5,6};
+  // int crr[6]={1,2,3,4,5,6};
+  // int brr[10]={1,2,3,12,8,6};
+
+// error 
+  // int drr[5]={1,2,3,4,5,6};  // error because element's
+  // are 6 and in arr declared 5
+
+  // char eerr[10]={'a','b','c','d','e'};
+  // cout<<"\n"<<eerr[2]<<endl;
+
+  // // printing all values
+  // for(char index=0; index<5;index++){  // why index=0 , we can take i,j,k, etc so it is name.
+  //   cout<<eerr[index]<<" "<<endl;
+  // }
+
+  // cout<<"\nArray initialized successfully \n"<<endl;
+
+
+  // taking input 
+
+  int arr[10]={1,2,3,12,8,6};
+  
+  return 0;
+}
