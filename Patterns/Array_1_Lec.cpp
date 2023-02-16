09c5e8dac77832e6614ad577160c5cdeb1151b2a
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-02-16 14:28:28 +0530

    update

--- Patterns/Array_1_Lec.cpp
+++ Patterns/Array_1_Lec.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
+#include<limits.h>
+#include<cstring>
+
 using namespace std;
 
+// bool find(int arr[],int size,int key){
+
+  // linear search in Array
+
+
+//   for(int i=0; i<size;i++){
+//   if(arr[i]==key)
+//   return true;
+// }
+// return false;
+// }
+
 int main(){
+
+// int arr[5]= {1,3,5,7,8};
+// int size=5;
+// cout<<"Enter the key to find"<<endl;
+
+// int key;
+// cin>>key;
+
+// if(find(arr,size,key)){
+//   cout<<"Found"<<endl;
+// }
+// else{
+//   cout<<"Not Found";
+// }
+
 
 
 // array initialized
@@ -27,7 +57,78 @@ int main(){
 
   // taking input 
 
-  int arr[10]={1,2,3,12,8,6};
+  // int arr[10]={1,2,3,12,8,6};
+
+
+  // memset   // wrong code
+
+  // int arr[10];
+  // memset(arr,1,sizeof(arr));
+
+  // for(int i=0; i<10; i++){
+  //   cout<<arr[i]<<" ";
+  // }
   
-  return 0;
+        // count 0's and 1's
+
+// int arr[]={0,1,0,1,0,1,0,1,0,1};
+// int size=10;
+
+// int numzero=0;
+// int numone=0;
+
+// for(int i=0; i<size;i++){
+//   if(arr[i]==0){
+//   numzero++;
+// }
+// if(arr[i]==1){
+//   numone++;
+// }
+// }
+// cout<<"numer of zeros :"<<numzero<<endl;
+// cout<<"number of ones :"<<numone<<endl;
+
+
+        //  find maximum number by using linear search in array
+
+
+// int arr[]={1,2,3,4,6,8,10,16,25,35,45,55};
+// int size=12;
+
+// // initialise the maxi variable with the 
+// // minimum possible integer value
+
+// int maxi =INT_MIN;
+// for(int i=0;i<size;i++){
+//   if(arr[i]>maxi){
+//     // found a number greater than
+//     //  maxi and update maxi
+//     maxi=arr[i];
+//   }
+// }
+// cout<<"maximum number is :"<<maxi<<endl;
+
+
+
+      // Array reverse
+
+// int arr[]={1,2,3,4,6,8,10,16,25,35,45,55};
+// int size=12;
+
+// int start=0;
+// int end=size-1;
+
+// while(start<=end){
+//   // step 1
+//   swap(arr[start],arr[end]);
+//   // step 2
+//   start++;
+//   // step 3
+//   end--;
+// }
+// // printing array
+// for(int i=0; i<size;i++){
+//   cout<<arr[i]<<" ";
+// }
+
 }
