64030881ff14e2c5119c25b8f12e1dae98d1b374
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-01 13:30:17 +0530

    update

--- searchingANDsorting/findsqrtofNumberUsingBinarySearch.cpp
+++ searchingANDsorting/findsqrtofNumberUsingBinarySearch.cpp
@@ -28,7 +28,11 @@ mid=s+(e-s)/2;
 }
 return ans;
 }
+
+
 int main(){
+
+// find sqrt in integer not in float
 
 int n;
 cout<<"Enter the number "<<endl;
@@ -38,6 +42,35 @@ int ans = findsqrt(n);
 
 cout<<"Ans is "<<ans<<endl;
 
+
 return 0;
+
+
+
+// find sqrt in float numbers using binary search 
+
+// int n;
+// cout<<"Enter the number "<<endl;
+// cin>>n;
+
+// int ans = findsqrt(n);
+
+// cout<<"Ans is "<<ans<<endl;
+
+// int precision;
+// cout<<"Enter number of floating digits in precision "<<endl; // how much number you want to print in float 
+// cin>>precision;
+
+// double step =0.1;
+// double finalans=ans;
+// for(int i=0; i<precision; i++){
+//   for(double j=finalans; j*j<=n; j=j+step){
+//     finalans=j;
+//   }
+//   step =step/10;
+// }
+// cout<<"Final ans is "<<finalans<<endl;
+
+// return 0;
 
 }
