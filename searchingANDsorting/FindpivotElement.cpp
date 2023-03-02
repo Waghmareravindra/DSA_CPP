d74315b9e977aeab465c8f771fe6fbdf5615fead
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-02 17:29:48 +0530

    last update

--- searchingANDsorting/FindpivotElement.cpp
+++ searchingANDsorting/FindpivotElement.cpp
@@ -6,22 +6,28 @@ using namespace std;
 
 // find pivot element---------------------
 
-int findpivot(vector<int> arr){
-  int s=0;
-  int e=arr.size()-1;
-  int mid=s+(e-s)/2;
+int findpivot(vector<int> arr) {
+  int s = 0;
+  int e = arr.size() - 1;
+  int mid = s + (e-s)/2;
 
-  while(s<=e){
-    if(mid+1 < arr.size() && arr[mid]>arr[mid+1])
-    return mid;
-    if(mid-1>=0 && arr[mid-1]>arr[mid])
-    return mid-1;
+  while(s <= e) {
+    if(s == e) {
+      //single element
+      return s;
+    }
+    if(mid <= e && arr[mid] > arr[mid+1])
+      return mid;
+    if(mid-1 >= s && arr[mid-1] > arr[mid])
+      return mid-1;
 
-    if(arr[s]>=arr[mid])
-     e=mid-1;
-     else
-     s=mid+1;
-     mid=s+(e-s)/2;
+    if(arr[s] > arr[mid]) 
+      e = mid - 1;
+    else
+      s = mid + 1;
+
+    mid = s + (e-s)/2;
+
   }
   return -1;
 }
@@ -30,10 +36,11 @@ int main(){
 
   // find pivot element in array----------------
 
- vector<int> arr{1,2,3,4,5,0,7};
+ vector<int> arr{1,2,3,4,5,6,7,8,9,0,15};
  int ans = findpivot(arr);
 
- cout<<"pivot element is "<<ans<<endl;
+ cout<<"pivot element is "<<arr[ans]<<endl;
+ cout<<"pivot element index is "<<ans<<endl;
  return 0;
 
 
