20db016a482d31dad5e1214f7cdb5aa352aa0a96
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-05 14:21:22 +0530

    update

---
+++ Char_Array_String_Class_1/removeAllDuplicate_adjacent.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+
+int main()
+{
+
+  // // Char AND Array Difference AND Called
+
+  // char arr[]={'a','b','c','d','e'};
+  // char str[]="hello"; std::string ravindra="hallow diamond";
+
+  // // cout<<arr[3];
+  // cout<<ravindra;
+
+  // // leet code question remove all adjacent duplicates in string
+
+/*it will run on leet code only if we want to run on vs code then you need to string the string */
+
+
+//     class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         string ans ="";
+//         int i = 0;
+
+//         while( i < s.length() ) {
+
+//                 if( ans.length() > 0  &&  ans[ans.length() - 1] == s[i]) {
+//                     ans.pop_back();
+//                 }
+//                 else {
+//                     ans.push_back(s[i]);
+//                 }
+//             i++;
+//         }
+//         return ans;
+//     }
+// };
+
+}
--- Char_Array_String_Class_1/tempCodeRunnerFile.cpp
+++ Char_Array_String_Class_1/tempCodeRunnerFile.cpp
@@ -1,10 +1,1 @@
-int getlength(char name[]){
-  int length =0;
-  int i=0;
-
-  while(name[i] != '\0'){
-    length++;
-    i++;
-  }
-  return length;
-}
+s="hello"
