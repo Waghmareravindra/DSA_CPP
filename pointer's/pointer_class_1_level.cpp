0776bb74b52801f4f7123e3b3262ce6830f1f89e
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-10 09:58:41 +0530

    update

--- pointer's/pointer_class_1_level.cpp
+++ pointer's/pointer_class_1_level.cpp
@@ -4,11 +4,28 @@ using namespace std;
 int main()
 {
 
-  int a = 5;
-  int b = 5;
+  // int a = 5;
+  // int b = 5;
 
-  cout << a << endl;
-  cout << &a << endl;
-  cout << b << endl;
-  cout << &b << endl;
+  // cout << a << endl;
+  // cout << &a << endl;
+  // cout << b << endl;
+  // cout << &b << endl;
+
+    int a=5;
+    // pointer create
+    int *ptr=&a;
+
+
+  // access the value ptr is pointing to
+
+  cout<<"address of a is "<<&a<<endl; // address of a
+  cout<<"value ptr is pointing to is "<<*ptr<<endl;  // value access not address
+  cout<<"value stored at ptr is "<<ptr<<endl;  // access of address of value
+  cout<<"address of ptr is "<<&ptr<<endl;
+
+
+
+
+
 }
