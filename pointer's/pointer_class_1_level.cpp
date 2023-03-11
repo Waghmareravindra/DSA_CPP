16bc749d7436a76c7f7dae398dc6cb2972d14bc5
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-11 12:37:01 +0530

    last update

--- pointer's/pointer_class_1_level.cpp
+++ pointer's/pointer_class_1_level.cpp
@@ -12,20 +12,82 @@ int main()
   // cout << b << endl;
   // cout << &b << endl;
 
-    int a=5;
-    // pointer create
-    int *ptr=&a;
+    // int a=5;
+    // // pointer create
+    // int *ptr=&a;
 
 
   // access the value ptr is pointing to
 
-  cout<<"address of a is "<<&a<<endl; // address of a
-  cout<<"value ptr is pointing to is "<<*ptr<<endl;  // value access not address
-  cout<<"value stored at ptr is "<<ptr<<endl;  // access of address of value
-  cout<<"address of ptr is "<<&ptr<<endl;
+  // cout<<"address of a is "<<&a<<endl; // address of a
+  // cout<<"value ptr is pointing to is "<<*ptr<<endl;  // value access not address
+  // cout<<"value stored at ptr is "<<ptr<<endl;  // access of address of value
+  // cout<<"address of ptr is "<<&ptr<<endl;
 
 
+// //pointer alway's store address of value 
 
+// find size of value it doesn't matter is it int or char or any other dat type it is 4 or 8 size of data type  
+
+// int a=5;
+// int *ptr=&a;
+
+// cout<<sizeof(ptr)<<endl;
+
+
+// char ch='c';
+// char *dtr=&ch;
+
+// cout<<sizeof(dtr)<<endl;
+
+// double pointer=5.03;
+// double *potr=&pointer;
+
+// cout<<sizeof(potr)<<endl;
+
+
+// // BAD PRACTICE
+// int *ptr;
+
+// cout<<*ptr<<endl;
+
+
+//  NULL POINTER
+// int *ptr=0;
+
+// cout<<*ptr<<endl;
+
+// --------------------------- //
+
+// int a=5;
+// int* ptr=&a;
+
+// // copy pointer
+// int* dusraptr=ptr;
+
+// cout<<*ptr<<endl;
+// cout<<*dusraptr<<endl;
+
+// cout<<*dusraptr*2<<endl;
+// cout<<*ptr*2<<endl;
+
+int a=5;
+int *p=&a;
+int *q=p;
+int *r=q;
+
+cout<<a<<endl;
+cout<<&a<<endl;
+cout<<*p<<endl;
+cout<<p<<endl;
+cout<<&p<<endl;
+cout<<r<<endl;
+cout<<&r<<endl;
+cout<<*r<<endl;
+cout<<(*p/2)<<endl;
+cout<<(*q/2)<<endl;
+cout<<(*r/2)<<endl;
+cout<<&r<<endl;
 
 
 }
