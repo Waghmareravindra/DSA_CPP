e78ac09cca6bd164778b10041d4b9a2b65d4255a
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-12 09:49:01 +0530

    .

---
+++ pointer's/Basic_mathematic_for_dsa.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  // int arr[5]={1,2,3,4,5};
+  // int *ptr=arr;
+  // ptr++; // value stored after increment 
+  // cout<<*ptr<<endl;// that's why printing 2 
+
+// LEET CODE QUESTION
+
+// Count Prime
+
+
+}
