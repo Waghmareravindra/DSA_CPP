233a9de733c73bc19f3d345b431a1ccb8e45540e
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-13 12:14:16 +0530

    update

--- pointer's/Basic_mathematic_for_dsa.cpp
+++ pointer's/Basic_mathematic_for_dsa.cpp
@@ -5,15 +5,30 @@ using namespace std;
 
 int fastexponentiation(int a, int b){
       int ans=1;
+      while(b>0){
+        if(b & 1){
+        ans*=a;
+      }
+      a=a*a;
+      b>>=1;
+  }
+    return ans;
+}// Time Complexity is O(log b)
+
+
+int slowexponentiation(int a, int b){
+      int ans=1;
       for(int i=0; i<b;++i){
         ans*=a;
       }
       return ans;
-}
+} // Time Complexity is O(b)
 
 int main(){
 
-  cout<<fastexponentiation(2,2);
+// fast exponentiation 
+  cout<<slowexponentiation(2,5);
+  cout<<fastexponentiation(2,5);
 
   // int arr[5]={1,2,3,4,5};
   // int *ptr=arr;
