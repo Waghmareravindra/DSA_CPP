4a7df4688174156ad79e53d7fe549562eabcaccf
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-13 08:20:25 +0530

    update

--- pointer's/Basic_mathematic_for_dsa.cpp
+++ pointer's/Basic_mathematic_for_dsa.cpp
@@ -9,8 +9,65 @@ int main(){
   // cout<<*ptr<<endl;// that's why printing 2 
 
 // LEET CODE QUESTION
-
 // Count Prime
 
+// above is wrong code written check at the end of the code which written right with time complexity O(n)
 
+/*
+class Solution {
+public:
+
+    bool isprime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    int countPrimes(int n) {
+        int c=0;
+
+        for(int i=0;i<n;i++){
+            if(isprime(i)){
+                ++c;
+            }
+        }
+        return c;
+    }
+};
+
+time complexity very high reduce time complexity write another code
+*/
+
+
+// LEET CODE QUESTION WITH O(n) TIME COMPLEXITY  WHICH IS RIGHT CODE REDUCE'D COMPLEXITY 
+
+class Solution {
+public:
+    int countPrimes(int n) {
+       if(n==0) 
+       return 0;
+
+       vector<bool> prime(n, true); // all are prime maked already
+       prime[0]=prime[1]=false;
+
+       int ans=0;
+       for(int i=2; i<n; i++){
+           if(prime[i]){
+               ans++;
+
+               int j=2*i;
+               while(j<n){
+                   prime[j]=false;
+                   j+=i;
+               }
+           }
+       }
+       return ans;
+    }
+};
 }
