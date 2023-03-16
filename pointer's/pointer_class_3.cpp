a620057647dc5db43e3bea8f6ac2680456cbbafd
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-03-16 19:42:16 +0530

    pointer class 3__ONLY all code in one last update

--- pointer's/pointer_class_3.cpp
+++ pointer's/pointer_class_3.cpp
@@ -1,26 +1,170 @@
-#include <iostream>
-using namespace std;
+// Pointer class (main(7))
 
-int main()
-{
+// #include <iostream>
+// using namespace std;
 
-  int a = 5;
-  int *p = &a;
-  int **q = &p;
+// int main() {
+//         int a = 5;
+//         int* p = &a;
+
+//         int** q = &p;
+        
+//         cout << &a << endl;
+//         cout << a << endl;
+//         cout << p << endl;
+//         cout << &p << endl;
+//         cout << *p << endl;
+//         cout << q << endl;
+//         cout << &q << endl;
+//         cout << *q << endl;
+//         cout << **q << endl;
+
+//   return 0;
+// }
 
 
-  cout << &a << endl;
-  cout << a << endl;
-  cout << p << endl;
-  cout << &p << endl;
-  cout << q << endl;
-  cout << &q << endl;
-  cout << *q << endl;
-  cout << **q << endl;
+
+
+// pointer class (main(8))
+
+// #include <iostream>
+// using namespace std;
+
+// void util(int* p )
+// {
+//         // p = p + 1;
+//         *p = *p + 1;
+// }
+
+// int main() {
+//   int a = 5;
+
+//   int* p = &a;
+
+//   cout << "before" << endl;
+//   cout << a << endl;
+//   cout << p << endl;
+//   cout << *p << endl;
+
+//   util(p);
+
+//   cout << endl << "after" << endl;
+//   cout << a << endl;
+//   cout << p << endl;
+//   cout << *p << endl;
+//   return 0;
+// }
+
+
+// Pointer class (main(9))
+
+
+// #include <iostream>
+// using namespace std;
+
+// void solve(int** ptr) {
+//         //ptr = ptr + 1;
+
+//         //*ptr = *ptr + 1;
+
+//         **ptr = **ptr + 1;
+// }
+
+// int main() {
+//         int x = 12;
+//         int* p = &x;
+//         int** q = &p;
+//         solve(q);
+
+//         cout << x << endl;
+
+//   return 0;
+// }
 
 
 
+// pointer (main(10))
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   int a = 5;
+//   int* p = &a;
+//   int** ptr = &p;
   
+//   **ptr = **ptr + 1;
 
-}
+
+//   return 0;
+// }
+
+
+// pointer (main(11))
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//         int a = 5;
+//         //creating a reference variable
+//         int& b = a;
+
+//         cout << a << endl;
+//         cout << b << endl;
+
+//         a++;
+//         cout << a << endl;
+
+//         cout << b << endl;
+
+//         b++;
+//         cout << a << endl;
+//         cout << b << endl;
+
+//   return 0;
+// }
+
+
+// pointer (main(12))
+
+
+// #include <iostream>
+// using namespace std;
+
+// void solve(int& num ) {
+//         num  = 50;
+// } 
+
+// void solve(int* val) {
+//         *val = *val + 1;
+// }
+
+// void solve(int*& p) {
+//         p = p + 1;
+// }
+
+// int main() {
+
+        // int a = 5;
+        // solve(a) ;
+
+        // cout << a << endl;
+
+        // int a = 12;
+        // int *p = &a;
+        // solve(p);
+        // cout << a << endl;
+
+        // int a = 5;
+        // int* p = &a;
+
+        // cout << "before " << p << endl; 
+        // solve(p);
+        // cout << "After " << p << endl;
+
+
+
+//   return 0;
+// }
--- pointer's/tempCodeRunnerFile.cpp
+++ pointer's/tempCodeRunnerFile.cpp
@@ -1,20 +1,3 @@
-int arr[10]={1,2,3,4};
-// cout<<"size inside main function is--> "<<sizeof(arr)<<endl;
 
-// cout<<arr<<endl;
-// cout<<&arr<<endl;
-// // printing array inside main
-
-// for(int i=0; i<10; i++){
-//   cout<<arr[i]<<" ";
-// }
-// cout<<endl;
-// cout<<endl<<endl<<"Now calling solve function"<<endl;
-
-// solve(arr);
-
-// cout<<"Wapas main function me aagye h "<<endl;
-
-// for(int i=0; i<10; i++){
-//   cout<<arr[i]<<" ";
-// }
+// solve(a);
+// cout<<a<<endl;
