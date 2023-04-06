100c05474d534c98997b76f91b7cb83cfc2fc4dd
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-04-07 00:05:34 +0530

    update

--- Patterns/triangele.cpp
+++ Patterns/triangele.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
+// int main(){
 
-  int n=5;
-  for(int row=0;row<n;row++){
-    for(int col=0;col<row+1;col++){
-      cout<<" ";
-    }
-    for(int col=0;col<n-row;col++){
-      cout<<"* ";
-    }
-    cout<<endl;
-  }
+//   int n=5;
+//   for(int row=0;row<n;row++){
+//     for(int col=0;col<row+1;col++){
+//       cout<<" ";
+//     }
+//     for(int col=0;col<n-row;col++){
+//       cout<<"* ";
+//     }
+//     cout<<endl;
+//   }
 
-}
+// }
