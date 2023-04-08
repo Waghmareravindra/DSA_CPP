7f1c4309a753566a95e8921c8e129efb6bda1230
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-04-08 16:54:26 +0530

    .

--- Patterns/tempCodeRunnerFile.cpp
+++ Patterns/tempCodeRunnerFile.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
-using namespace std;
+for(int row=0; row<n; row++){
 
-int main(){
-
-  int n=5;
-  for(int row=0;row<n;row++){
-    for(int col=0;col<row+1;col++){
-      cout<<" ";
-    }
-    for(int col=0;col<n-row;col++){
-      cout<<"* ";
-    }
-    cout<<endl;
-  }
+//   for(int col=0; col<n-row-1; col++){
+//     cout<<" ";
+//   }
+//   for(int col=0; col<2*row+1; col++){
+//     if(col==0||col==2*row){
+//       cout<<"*";
+//     }
+//     else{
+//       cout<<" ";
+//     }
+//   }
+//    cout<<endl;
+//   }
