369df81ffb3636aac5ab7d2a012f9ef6aaed11dc
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-05-29 18:33:43 +0530

    fibonacci number upto number

--- Patterns/fibonacci_series.cpp
+++ Patterns/fibonacci_series.cpp
@@ -1,18 +1,20 @@
-#include<iostream.h>
+#include <iostream>
 using namespace std;
 
-int main(){
-  int n, x,y,z;
-  cout<<"Enter Max Term: ";
-  cin>>n;
-  x=0;
-  y=1;
-  z=0;
-  while(z<=n){
-    x=y;
-    y=z;
-    z=x+y;
-    cout<<z;
+int main()
+{
+  int n, x, y, z;
+  cout << "Enter Max Term: ";
+  cin >> n;
+  x = 0;
+  y = 1;
+  z = 0;
+  while (z <= n)
+  {
+    x = y;
+    y = z;
+    z = x + y;
+    cout << z;
   }
   return 0;
 }
