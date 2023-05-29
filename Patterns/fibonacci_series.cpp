3f2d59f25b7150e3b0fec5a39e0818a9949eb1d6
Waghmareravindra <100698590+Waghmareravindra@users.noreply.github.com>
2023-05-29 18:31:37 +0530

    .

---
+++ Patterns/fibonacci_series.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+using namespace std;
+
+int main(){
+  int n, x,y,z;
+  cout<<"Enter Max Term: ";
+  cin>>n;
+  x=0;
+  y=1;
+  z=0;
+  while(z<=n){
+    x=y;
+    y=z;
+    z=x+y;
+    cout<<z;
+  }
+  return 0;
+}
